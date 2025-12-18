c1c67180f5d39c423a0a5eaebccea9ff31de83e8
Abinesh <abinesha47@gmail.com>
2025-12-18 15:14:29 +0530

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/layout/Footer";
 import { Header } from "./components/layout/Header";
 import { Container } from "./components/reusable/Container";
 import { Dashboard } from "./pages/Dashboard";
+import { Page1 } from "./pages/Page1";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-backdrop-primary font-sans">
-      <Header />
-      <Container>
-        <Dashboard />
-      </Container>
-      <Footer />
-    </div>
+    <BrowserRouter basename="/react-app-governance-ui">
+      <div className="min-h-screen bg-backdrop-primary font-sans flex flex-col">
+        <Header />
+        <Container className="flex-1">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="page1" element={<Page1 />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
