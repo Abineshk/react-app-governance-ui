9af018d967f615ad827f56ea9bb959a53afd173c
Abinesh <abinesha47@gmail.com>
2025-12-18 09:42:48 +0530

    Updated deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"/react-app-governance-ui/",
   build: {
     target: "esnext",
     outDir: "build",
